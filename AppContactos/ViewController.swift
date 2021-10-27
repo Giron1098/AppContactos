f67731282d8bb80fd25e778631c45550aca2fb8f
Ricardo Giron Colorado <ricardo.giron.colorado@gmail.com>
2021-10-27 17:14:35 -0500

    Inicio de App

---
+++ AppContactos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppContactos
+//
+//  Created by Mac4 on 27/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
